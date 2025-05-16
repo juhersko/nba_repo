186177f03608fc685e6af361f72e49b64dfe0b82
Justin Herskowitz <juhersko@dhcp-10-173-208-209.umd.edu>
2025-05-16 14:46:15 -0400

    initial commit

--- index.js
+++ index.js
@@ -5,12 +5,41 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = 3000;
 
+
+const port = 3000;
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.get('/', (req, res) => {
+    res.sendFile('public/home.html', { root: __dirname });
+});
+app.get('/home', (req, res) => {
+    res.sendFile('public/home.html', { root: __dirname });
+});
+app.get('/about', (req, res) => {
+    res.sendFile('public/about.html', { root: __dirname });
+});
+app.get('/player', (req, res) => {
+    res.sendFile('public/player.html', { root: __dirname });
+});
+app.get('/ranking', (req, res) => {
+    res.sendFile('public/ranking.html', { root: __dirname });
+});
+app.get('/top_teams_per_year', (req, res) => {
+    res.sendFile('public/top_teams_per_year.html', { root: __dirname });
+});
+app.get('/teams', (req, res) => {
+    res.sendFile('public/teams.html', { root: __dirname });
+});
+
+
+
+
+
+
 
 app.post('/rankings', async (req, res) => {
   const { player_name, rating } = req.body;
